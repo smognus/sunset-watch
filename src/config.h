9e0f2d664cfa3b71b51aa0c19019ac87ce72193e
Mikey Coulson <miketcoulson@gmail.com>
2013-08-14 17:46:49 -0700

    Gave config.h a sane TIMEZONE value for the coordinate values.

--- src/config.h
+++ src/config.h
@@ -1,6 +1,6 @@
 //NOTE: longitude is positive for East and negative for West
 #define LATITUDE    33.6751
 #define LONGITUDE -112.096
-#define TIMEZONE 5;
+#define TIMEZONE -7;
 //NOTE: Change false to true if you want to enable the vibe function
 #define HOUR_VIBRATION false
