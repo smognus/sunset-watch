628afbcfbd11294f8449dd4d807321686522efef
BBPunjabi816 <bajwa.balraj@yahoo.com>
2013-09-14 11:00:50 -0700

    Update config.h

--- src/config.h
+++ src/config.h
@@ -1,6 +1,6 @@
 //NOTE: longitude is positive for East and negative for West
-#define LATITUDE    33.6751
-#define LONGITUDE -112.096
+#define LATITUDE    37.5483
+#define LONGITUDE -121.9886
 #define TIMEZONE -7;
 //NOTE: Change false to true if you want to enable the vibe function
-#define HOUR_VIBRATION false
+#define HOUR_VIBRATION true
